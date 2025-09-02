4a608417817e2e444593ec40859bab051012a09f
JonEG <echevestegonzalezjon@gmail.com>
2025-09-02 12:38:51 +0200

    test: green for out of stock product

--- src/inventory/forManagingProducts/getCurrentStock/GetCurrentStockHandler.ts
+++ src/inventory/forManagingProducts/getCurrentStock/GetCurrentStockHandler.ts
@@ -4,6 +4,13 @@ import { GetCurrentStockResponse } from "./GetCurrentStockResponse";
 
 export class GetCurrentStockHandler {
     handle(query: GetCurrentStock): GetCurrentStockResponse {
+        if (!this.hasStockOf(query.productId)) {
+            return GetCurrentStockResponse.withError(`Product with id ${(query.productId)} is out of stock`)
+        }
         return GetCurrentStockResponse.withError(`Product with id ${(query.productId)} does not exist`)
+    }
+
+    private hasStockOf(productId: String): boolean {
+        return productId != 'out-of-stock-product-id'
     }
 }
