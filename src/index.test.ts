6695025bd0294168c32fe83bebdd6d241807c050
JonEG <echevestegonzalezjon@gmail.com>
2025-09-04 13:31:42 +0200

    test: red for invalid parameters

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,8 @@
 import { describe, it, expect } from 'vitest';
 import { GetCurrentStock } from './inventory/forManagingProducts/getCurrentStock/GetCurrentStock';
 import { GetCurrentStockHandler } from './inventory/forManagingProducts/getCurrentStock/GetCurrentStockHandler';
+import { InvalidIdentifierError } from "./inventory/forRetrievingProducts/InvalidIdentifierError";
+import { ProductId } from "./inventory/product/ProductId";
 
 describe('For Managing Products Port', () => {
     describe('When we ask the current stock of a not existing product', () => {
@@ -32,6 +34,20 @@ describe('For Managing Products Port', () => {
                 name: 'In of Stock Product',
                 stock: 10
             })
+        })
+    })
+    describe('When we provide invalid parameters', () => {
+        it('Should fail with Invalid Argument Error', () => {
+            const query = new GetCurrentStock('')
+            const handler = new GetCurrentStockHandler()
+            const response = handler.handle(query)
+            expect(response.success()).toBeFalsy()
+            expect(response.error()).toEqual("Invalid product id")
+        })
+    })
+    describe('ProductId', () => {
+        it('Should not be instantiated empty', () => {
+            expect(() => {ProductId.validFrom('')}).toThrowError(InvalidIdentifierError)
         })
     })
 })
