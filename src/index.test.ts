58fb33eb307795e0c4849eebc632a0d5af07f850
JonEG <echevestegonzalezjon@gmail.com>
2025-09-05 12:36:10 +0200

    test: red, for stock of an exising product

---
+++ src/index.test.ts
@@ -0,0 +1,21 @@
+import { describe, it, expect } from 'vitest';
+
+function BuildGetCurrentStockHandler(): GetCurrentStockHandler {
+    return new GetCurrentStockHandler()
+}
+
+describe('For Managing Products Port', () => {
+    describe('When we ask the current stock of an existing product', () => {
+        it('Should return a product stock object as response with available units', () => {
+            const query = new GetCurrentStock('existing-product-id')
+            const handler = BuildGetCurrentStockHandler()
+            const result = handler.handle(query)
+            const stock = result.unwrap()
+            expect(stock).toEqual({
+                id: 'existing-product-id',
+                name: 'existing-product-name',
+                stock: 10
+            })
+        })
+    })
+})
