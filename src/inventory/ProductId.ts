97d613f9887cc7d9aa5d4099bbbb8dff4009c015
JonEG <echevestegonzalezjon@gmail.com>
2025-09-12 21:54:40 +0200

    test: green for invalid product id

--- src/inventory/ProductId.ts
+++ src/inventory/ProductId.ts
@@ -2,8 +2,8 @@ export class ProductId {
     private constructor(private id: string) { }
 
     static validatedFrom(id: string): ProductId {
-        if (!id || id.trim() === '') {
-            throw new Error('Invalid product ID');
+        if (id.length === 0) {
+            throw new Error(`Invalid product id [${id}]`);
         }
         return new ProductId(id);
     }
