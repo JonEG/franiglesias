e4dd62fc55a9490b4d6a09816964ac95d54876b6
JonEG <echevestegonzalezjon@gmail.com>
2025-09-15 19:17:15 +0200

    test: green for registering products with invalid name

--- src/inventory/driving/forManagingProducts/addProduct/AddProductResponse.ts
+++ src/inventory/driving/forManagingProducts/addProduct/AddProductResponse.ts
@@ -5,4 +5,18 @@ export class AddProductResponse {
     unwrap(): string {
         return this.result;
     }
+
+    error() {
+        return new InvalidProductName('');
+    }
+}
+
+export class InvalidProductName implements Error {
+    name: string;
+    message: string;
+
+    constructor(productName: string) {
+        this.name = 'INVALID_PRODUCT_NAME';
+        this.message = `[${productName}] is not a valid name`
+    }
 }
